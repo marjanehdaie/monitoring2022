ed21b90e99b8a25e5c2f302333d04b3beaf8d99b
marjanehdaie <115725590+marjanehdaie@users.noreply.github.com>
2022-10-13 18:00:51 +0200

    Create R_code_ecointeract.r

---
+++ R/R_code_ecointeract.r
@@ -0,0 +1,9 @@
+ # this is a code for investigating relationships among ecological variables
+ 
+# we are using the sp package . to install is use library 
+# install.packages("sp")
+library(sp)
+
+# recall dataset using function data 
+data(meuse)
+
