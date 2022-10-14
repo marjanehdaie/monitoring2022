c2b10e3ef53a2c4bc452db61e8f5221a6a7122bf
marjanehdaie <115725590+marjanehdaie@users.noreply.github.com>
2022-10-14 17:25:13 +0200

    Update R_code_ecointeract.r

--- R/R_code_ecointeract.r
+++ R/R_code_ecointeract.r
@@ -7,3 +7,9 @@ library(sp)
 # recall dataset using function data 
 data(meuse)
 
+# you can aslo make use of require()
+# we are using meuse # search for "meuse dataset R sp package"
+# https://cran.r-project.org/web/packages/gstat/vignettes/gstat.pdf
+data(meuse)
+
+view(meuse)
