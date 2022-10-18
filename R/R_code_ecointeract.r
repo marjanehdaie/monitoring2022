cc87db1ab4cfb95fc8496c453b25a91d00655925
marjanehdaie <115725590+marjanehdaie@users.noreply.github.com>
2022-10-18 22:17:21 +0200

    Update R_code_ecointeract.r

--- R/R_code_ecointeract.r
+++ R/R_code_ecointeract.r
@@ -13,3 +13,26 @@ data(meuse)
 data(meuse)
 
 view(meuse)
+dev.off()
+Error in dev.off() : cannot shut down 
+summary(meuse)
+
+plot(cadmium, zinc) # error
+plot(meuse$cadmium,meuse$zinc)
+cad <- meuse$cadmium
+zin <- meuse$zinc
+plot(cad,zin)
+attach(meuse)
+plot(cadmium,zinc)
+# detach(meuse) 
+pairs(meuse)
+meuse[ ,3: 6]
+pol <- meuse[,3: 6]
+pol 
+head (pol)
+pairs(pol, col="blue", cex=1,5)
+pairs (cadmium + copper + lead + zinc + data=meuse )
+coordniates(meuse)
+coordinates(meuse) = ~x+y
+spplot(meuse, "cadmium" , main="concentration of cadmium")
+spplot(meuse, c("copper","zinc"))
